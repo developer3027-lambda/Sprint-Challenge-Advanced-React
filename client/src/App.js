73b5a491aa06698e6acf73da15cd9471450e2892
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2019-11-10 22:33:58 -0500

    map names from data onscreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+constructor() {
+  super()
+  this.state = {
+    players:[],
+  }
+}
 
+componentDidMount() {
+  axios
+    .get(`http://localhost:5000/api/players`)
+    .then(res => {
+      console.log(res);
+      this.setState({
+        players: res.data
+      });
+    })
+    .catch(err => console.log('axios err: ', err));
+}
   render(){
     return (
       <div className="App">
-        <h1>Yep</h1>
+      {this.state.players.map(player => (
+        <div key={player.id}>
+          <h3>{player.name}</h3>
+        </div>
+      ))}
+        
       </div>
     );
   }
